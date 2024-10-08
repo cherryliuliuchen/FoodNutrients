d62b51ac7d3ee637483608c12ba9010e805cfd55
CherryLiu <cherryliuliuchen@gmail.com>
2024-10-08 13:14:14 +0200

    Deleted all files from main

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer = () => (
-  <footer className="custom-footer">
-    <p>&copy; 2024 MyFoodApp. All rights reserved.</p>
-  </footer>
-);
-
-export default Footer;
--- src/components/HomePage/Recommend.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Recommend = () => (
-  <div>
-    <h2>Recommended Foods</h2>
-  </div>
-);
-
-export default Recommend;
