70b27c98f5a00b47577155c9b08cdbc74c3d392d
Cherry Chen Liu <105421663+cherryliuliuchen@users.noreply.github.com>
2024-10-09 14:54:22 +0200

    Update apiConfig.js

--- frontend-user/src/api/apiConfig.js
+++ frontend-user/src/api/apiConfig.js
@@ -1,4 +1,3 @@
 // src/api/apiConfig.js
 // export const API_BASE_URL = 'http://localhost:5000/api';
 export const API_BASE_URL = 'https://foodapp-cqdcbbc6d8gqambb.germanywestcentral-01.azurewebsites.net/api';
-// export const USDA_API_KEY = '2Si9Mhg7EhQkRqF72ULhkdtOghdO2h8LbRkVjISr';
